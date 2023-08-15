831476103ccbf04cfae00a56847f033fce14a42c
Ankit Yadav <119873198+ANKITy102@users.noreply.github.com>
2023-08-16 01:48:54 +0530

    all routes related to blog completed
    like blog, dislike blog, delete blog, update blog all routes are completed

--- backend/routes/blogRoute.ts
+++ backend/routes/blogRoute.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import protect from "../middleware/authMiddleware";
-import { createBlog } from "../controller/blogController";
+import { createBlog, dislikeBlog, likeBlog, updateBlog } from "../controller/blogController";
 
  const router = express.Router();
 
 router.post("/create", protect, createBlog)
+router.post("/update", protect, updateBlog)
+router.post("/likeblog", protect, likeBlog)
+router.post("/dislikeblog", protect, dislikeBlog)
 export default router;
