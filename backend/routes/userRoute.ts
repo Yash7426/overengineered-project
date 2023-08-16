448ec7656a2c5879372ce9a80b5db5f2bbe9bd66
Ankit Yadav <119873198+ANKITy102@users.noreply.github.com>
2023-08-16 22:24:57 +0530

    changes in userRoute

--- backend/routes/userRoute.ts
+++ backend/routes/userRoute.ts
@@ -3,5 +3,5 @@ import {loginUser, registerUser } from "../controller/userController"
  const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login", loginUser)
+router.post("/login", loginUser);
 export default router;
