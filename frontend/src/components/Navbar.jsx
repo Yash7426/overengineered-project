d2cfa0d07a83ff3ee851ee62612caf3b3db83b1f
Yash7426 <lcs2022057@iiitl.ac.in>
2023-08-19 18:44:22 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,6 +15,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [state, setState] = useState(false);
   const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const userId = sessionStorage.getItem("userId");
   function handleLogout() {
     setIsLoggingOut(true)
     axios
@@ -33,6 +34,9 @@ const Navbar = () => {
         console.log(err);
         setIsLoggingOut(false)
       });
+  }
+  function handleLogin(){
+    navigate("/signup")
   }
   const navigation = [
     { title: "Create Blog", path: "/dashboard/add" },
@@ -108,11 +112,11 @@ const Navbar = () => {
             })}
             <li>
               <button
-                onClick={handleLogout}
+                onClick={userId?handleLogout:handleLogin}
                 disabled={isLoggingOut}
                 className="block py-2 px-4 min-w-[40px] min-h-[20px] font-medium text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow md:inline"
               >
-                {isLoggingOut? <BiLoaderCircle className="animate-spin"/> :"Sign Out"}
+                {isLoggingOut? <BiLoaderCircle className="animate-spin"/> : userId ?"Sign Out":"Sign In"}
               </button>
             </li>
           </ul>
